31455ec0b278b770fecdcc31ef7121634889ad24
Cristinaferza <50017622+Cristinaferza@users.noreply.github.com>
2019-05-10 17:28:59 +0200

    Add files via upload

--- web/js/main.js
+++ web/js/main.js
@@ -1,10 +1,53 @@
 
     $(document).ready(function(){
-        $(".desplegable").hide();
+        
+        /*Menu mobile*/
+            /*Opcion 1*/
+        /*$(".desplegable").hide();
         $(".mobile-menu").click(function(){
             $(".desplegable").toggle();
-        })
+        });*/
+        
+            /*Opcion 2*/
+        /*$(".desplegable").hide();
+        $(".mobile-menu").click(function(){
+            $(".desplegable").toggle();
+         })
+         $(".desplegable").mouseout(function(){
+            $(".desplegable").toggle();
+         })*/
+        
+            /*Opcion 3*/
+        /*$(".desplegable").hide();
+        $(".mobile-menu").click(function(){
+            $(".desplegable").toggle();
+         });
+        $(".box").click(function(){
+            $(".desplegable").toggle();
+        });*/
+         
+        /***********************************/
+        
+        function myFunction(x) {
+            var menu = $(".desplegable");
+            if (x.matches) {
+                menu.hide();
+                $(".mobile-menu").click(function(){
+                    menu.toggle();
+                 });
+                $(".box").click(function(){
+                    menu.hide();
+                });
+            } else {
+                menu.hide();
+            }
+        }
+        var x = window.matchMedia("(max-width: 640px)");
+        myFunction(x);
+        x.addListener(myFunction);
 
+        /**********************************/
+        /*Menu desktop*/
         $(".desplegable-desktop").hide();
         $(".mobile-desktop").click(function(){
             $(".desplegable-desktop").toggle();
