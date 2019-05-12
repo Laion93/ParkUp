d56a22589b77fa1a1f222707de8cd19e7adde0f8
JesusW90 <50017511+JesusW90@users.noreply.github.com>
2019-05-12 15:44:06 +0200

    Add files via upload

---
+++ src/java/controlador/OcuparPlaza.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import beans.Plaza;
+import beans.Usuario;
+import beans.Vehiculo;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import negocio.GestionPlazas;
+import negocio.GestionUsuarios;
+import negocio.GestionVehiculos;
+
+/**
+ *
+ * @author Manchita
+ */
+@WebServlet(name = "OcuparPlaza", urlPatterns = {"/OcuparPlaza"})
+public class OcuparPlaza extends HttpServlet {
+
+    GestionPlazas gestionPlazas;
+    GestionUsuarios gestionUsuarios;
+    GestionVehiculos gestionVehiculos;
+    
+    public OcuparPlaza() {
+        super();
+       gestionPlazas = new GestionPlazas();
+       gestionUsuarios = new GestionUsuarios();
+       gestionVehiculos = new GestionVehiculos();
+    }
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+                HttpSession sesion= request.getSession(false);
+		if(sesion!=null){
+
+                    int numeroPlaza = Integer.parseInt((String) sesion.getAttribute("numeroPlaza"));
+                    String matricula = request.getParameter("vehiculoSeleccionado");
+                    
+                    Plaza p = gestionPlazas.recuperarPlaza(numeroPlaza);
+                    Vehiculo v = gestionVehiculos.recuperarVehiculo(matricula);
+                    
+                    if(p.getTipoVehiculo().equals(v.getTipoVehiculo()) && p.isElectrico() == false){
+                        
+                        gestionPlazas.ocuparPlaza(numeroPlaza, v);
+
+                        response.sendRedirect("Plazas");
+                    }else if(p.getTipoVehiculo().equals("coche") && v.getTipoVehiculo().equals("coche") && p.isElectrico() == v.isElectrico()){
+                    
+                        gestionPlazas.ocuparPlaza(numeroPlaza, v);
+
+                        response.sendRedirect("Plazas");
+                    }else{
+                        String error1 = "El vehiculo elegido no es valido para la plaza seleccionada.";
+                        response.sendRedirect("reservaPlaza.jsp?error="+error1);
+                    }
+		}else{
+			response.sendRedirect("login.jsp?error=nologueado");
+		}
+    }
+    
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/java/controlador/RecuperarUsuariosAdmin.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import beans.Usuario;
+import beans.Vehiculo;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import negocio.GestionUsuarios;
+import negocio.GestionVehiculos;
+
+/**
+ *
+ * @author Manchita
+ */
+@WebServlet(name = "RecuperarUsuariosAdmin", urlPatterns = {"/RecuperarUsuariosAdmin"})
+public class RecuperarUsuariosAdmin extends HttpServlet {
+
+    GestionUsuarios gestionUsuarios;
+    GestionVehiculos gestionVehiculos;
+ 
+    public RecuperarUsuariosAdmin() {
+        super();
+        gestionUsuarios = new GestionUsuarios();
+        gestionVehiculos = new GestionVehiculos();
+    }
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        HttpSession session= request.getSession(false);
+        
+        if(session!=null){
+
+            ArrayList<Usuario> usuariosTabla = (ArrayList)gestionUsuarios.recuperarUsuarios();
+
+            ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+            
+            for (Usuario u : usuariosTabla){
+                ArrayList<Vehiculo> vehiculos = (ArrayList)gestionVehiculos.recuperarVehiculosDeUsuario(u);
+                u.setVehiculos(vehiculos);
+                usuarios.add(u);
+            }
+            
+            session.setAttribute("usuarios", usuarios);
+
+            response.sendRedirect("usuarios.jsp");
+            
+        }else{
+            response.sendRedirect("login.jsp");
+	}
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
