9302c20437313f68f4f9bd4a261fc68fb63c01a3
JesusW90 <50017511+JesusW90@users.noreply.github.com>
2019-05-18 18:40:58 +0200

    Add files via upload

---
+++ src/java/negocio/GestionPlazas.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package negocio;
+
+import beans.Plaza;
+import beans.Vehiculo;
+import dao.PlazaDao;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Manchita
+ */
+public class GestionPlazas {
+    
+    PlazaDao dao;
+	
+	public GestionPlazas(){
+		dao = new PlazaDao();
+	}
+
+	public boolean ocuparPlaza(int numeroPlaza, Vehiculo v) {
+		return dao.ocuparPlaza(numeroPlaza, v);
+	}
+
+	public boolean liberarPlaza(String nombrePlaza){
+		return dao.liberarPlaza(nombrePlaza);
+	}
+        
+        public ArrayList<Plaza> recuperarPlazas(){
+		return dao.recuperarPlazas();		
+	}
+        
+        public Plaza recuperarPlaza(int numeroPlaza){
+		return dao.recuperarPlaza(numeroPlaza);		
+	}
+        
+        public boolean modificarPlaza(Plaza p) {
+		return dao.modificarPlaza(p);
+	}
+        
+	public void salir(){
+		dao.cerrarConexion();
+	}
+}
