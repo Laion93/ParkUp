357d4d48464ae78c9d7331cd3cf5668530bcba5c
JesusW90 <50017511+JesusW90@users.noreply.github.com>
2019-05-21 01:06:23 +0200

    Add files via upload

---
+++ src/java/controlador/Perfil.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import beans.Usuario;
+import beans.Vehiculo;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import negocio.GestionVehiculos;
+
+/**
+ *
+ * @author informatica
+ */
+@WebServlet(name = "Perfil", urlPatterns = {"/Perfil"})
+public class Perfil extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+	
+    GestionVehiculos gestionVehiculo;
+ 
+    public Perfil() {
+        super();
+        gestionVehiculo = new GestionVehiculos();
+    }
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+            HttpSession session= request.getSession(false);
+            if(session!=null){
+                Usuario user = (Usuario)session.getAttribute("usuario");
+                
+                if((gestionVehiculo.recuperarVehiculosDeUsuario(user)).isEmpty()){
+                    response.sendRedirect("perfil.jsp?mensaje=NoTieneVehiculos");
+                }else{
+                    ArrayList<Vehiculo> vehiculos = (ArrayList)gestionVehiculo.recuperarVehiculosDeUsuario(user);
+                    
+                    session.setAttribute("vehiculos", vehiculos);
+
+                    response.sendRedirect("perfil.jsp");
+                }
+            }else{
+                response.sendRedirect("login.jsp?error=nologueado");
+            }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
